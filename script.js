88c7937ac8ab87c978889508278ebbfd0108ea84
gfike19 <gfike19@gmail.com>
2020-06-29 23:21:30 -0500

    removed unnecessary slash

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-\// Write your JavaScript code here!
+// Write your JavaScript code here!
 window.addEventListener("load", function () {
 
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
